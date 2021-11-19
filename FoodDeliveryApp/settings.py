6af09c726b8e9070dd4c0d51a311ccdbb8a750c8
komalkknk <78220140+komalkknk@users.noreply.github.com>
2021-11-19 13:33:40 +0530

    Update settings.py

--- FoodDeliveryApp/settings.py
+++ FoodDeliveryApp/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2q994jbj#@-^@u=n+3!u3*ib7a&)a%s#q2z%g*6z=zd5w$us=s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
